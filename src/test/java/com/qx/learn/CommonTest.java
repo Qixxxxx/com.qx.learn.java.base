5a82afd062595b1073005f45c83026c288792a6c
xin_qi <xin_qi@trip.com>
2023-08-14 17:21:06 +0800

    update

---
+++ src/test/java/com/qx/learn/CommonTest.java
@@ -0,0 +1,62 @@
+package com.qx.learn;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CommonTest {
+
+    @Test
+    public void test1() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
+        long startTime = System.currentTimeMillis();
+        list.add(0, 0);
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
+        long startTime1 = System.currentTimeMillis();
+        List<Integer> newList = new ArrayList<>();
+        newList.add(0);
+        newList.addAll(list1);
+        long endTime1 = System.currentTimeMillis();
+        System.out.println(endTime1 - startTime1);
+
+    }
+
+    private static void linkedListGet(int elementCount, int loopCount) {
+        List<Integer> list = IntStream.rangeClosed(1, elementCount).boxed().collect(Collectors.toCollection(LinkedList::new));
+        IntStream.rangeClosed(1, loopCount).forEach(i -> list.get(ThreadLocalRandom.current().nextInt(elementCount)));
+    }
+
+    private static void arrayListGet(int elementCount, int loopCount) {
+        List<Integer> list = IntStream.rangeClosed(1, elementCount).boxed().collect(Collectors.toCollection(ArrayList::new));
+        IntStream.rangeClosed(1, loopCount).forEach(i -> list.get(ThreadLocalRandom.current().nextInt(elementCount)));
+    }
+
+    private static void linkedListAdd(int elementCount, int loopCount) {
+        List<Integer> list = IntStream.rangeClosed(1, elementCount).boxed().collect(Collectors.toCollection(LinkedList::new));
+        IntStream.rangeClosed(1, loopCount).forEach(i -> list.add(ThreadLocalRandom.current().nextInt(elementCount), 1));
+    }
+
+    private static void arrayListAdd(int elementCount, int loopCount) {
+        List<Integer> list = IntStream.rangeClosed(1, elementCount).boxed().collect(Collectors.toCollection(ArrayList::new));
+        IntStream.rangeClosed(1, loopCount).forEach(i -> list.add(ThreadLocalRandom.current().nextInt(elementCount), 1));
+    }
+
+    private static void linkedListAddFirst(int elementCount, int loopCount) {
+        LinkedList<Integer> list = IntStream.rangeClosed(1, elementCount).boxed().collect(Collectors.toCollection(LinkedList::new));
+        IntStream.rangeClosed(1, loopCount).forEach(i -> list.addFirst(ThreadLocalRandom.current().nextInt(elementCount)));
+    }
+
+    private static void arrayListAddFirst(int elementCount, int loopCount) {
+        List<Integer> list = IntStream.rangeClosed(1, elementCount).boxed().collect(Collectors.toCollection(ArrayList::new));
+        IntStream.rangeClosed(1, loopCount).forEach(i -> list.add(0, ThreadLocalRandom.current().nextInt(elementCount)));
+    }
+}
