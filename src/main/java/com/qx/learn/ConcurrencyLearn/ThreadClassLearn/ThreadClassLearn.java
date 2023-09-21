ff9ebe51d8dada31de4a070c3d4939f83151e6a4
xin_qi <xin_qi@trip.com>
2023-09-21 16:08:09 +0800

    update

--- src/main/java/com/qx/learn/ConcurrencyLearn/ThreadClassLearn/ThreadClassLearn.java
+++ src/main/java/com/qx/learn/ConcurrencyLearn/ThreadClassLearn/ThreadClassLearn.java
@@ -3,15 +3,29 @@ package com.qx.learn.ConcurrencyLearn.ThreadClassLearn;
 /**
  *  继承Thread类，重写run方法创建线程。Thread类实现了Runnable接口，Thread类的run方法是实现了Runnable接口中的run方法
  *
- *  Thread生命周期：
+ *  Thread生命周期：NEW，RUNNABLE，BLOCKED，WAITING，TIMED_WAITING，Java中的阻塞有三种状态（BLOCKED，WAITING，TIMED_WAITING）
  *      NEW:初始状态，线程被创建，但是没有调用start()方法
- *      RUNNABLE：运行状态，java线程将操作系统中的就绪态、运行态笼统称为运行状态
- *      BLOCKED：阻塞状态，线程被阻塞
- *      WAITING：等待状态，线程进入等待，当前线程需要等待其他线程做特定动作
- *      TIMED_WAITING：超时等待状态，不同于WAITING，可以在指定的时间自行返回
- *      TERMINATED：终止状态，线程执行完毕
- *
- *
+ *      RUNNABLE：运行状态，对应着操作系统线程中的Running和Ready态，Java 中处于 Runnable 状态的线程有可能正在执行，也有可能没有正在执行，正在等待被分配 CPU 资源
+ *          如果一个正在运行的线程是 Runnable 状态，当它运行到任务的一半时，执行该线程的 CPU 被调度去做其他事情，导致该线程暂时不运行，它的状态依然不变，还是 Runnable
+ *      BLOCKED：被阻塞状态，线程没有拿到 monitor 锁（进入 synchronized 保护的代码时没有抢到 monitor 锁，无论是进入 synchronized 代码块，还是 synchronized 方法）
+ *      WAITING：等待状态，线程进入等待，当前线程需要等待其他线程做特定动作（比如 join 的线程执行完毕，或者是 notify()/notifyAll()）。
+ *          线程进入WAITING有三种可能：1.没有设置 Timeout 参数的 Object.wait() 方法；2.没有设置 Timeout 参数的 Thread.join() 方法；3.LockSupport.park() 方法
+ *      TIMED_WAITING：超时等待状态，与WAITING相似，区别仅在于有时间限制，可以在指定的时间后自动幻想，或者在超时前被唤醒信号唤醒
+ *          线程进入TIMED_WAITING有4种可能：1.设置了时间参数的 Thread.sleep(long millis) 方法；2.设置了时间参数的 Object.wait(long timeout) 方法；
+ *                                       3.设置了时间参数的 Thread.join(long millis) 方法；4.设置了时间参数的 LockSupport.parkNanos(long nanos) 方法和 LockSupport.parkUntil(long deadline) 方法
+ *      TERMINATED：终止状态，线程执行完毕run（）方法或者出现一个没有捕获的异常导致run方法被终止
+ *  Monitor机制：JVM给每个对象和class字节码都设置了一个监听器Monitor，用于检测并发代码的重入，同时在Object类中提供了notify和wait方法来对线程进行控制
+ *      在Object类中有如下代码：
+ *          public class Object {
+ *              private transient int shadow$_monitor_;
+ *              public final native void notify();
+ *              public final native void notifyAll();
+ *              public final native void wait() throws InterruptedException;
+ *              public final void wait(long millis) throws InterruptedException {
+ *                  wait(millis, 0);
+ *              }
+ *              public final native void wait(long millis, int nanos) throws InterruptedException;
+ *          }
  *
  *  Thread中常用方法：
  *      1.start():启动当前线程；调用当前线程的run()
@@ -20,7 +34,7 @@ package com.qx.learn.ConcurrencyLearn.ThreadClassLearn;
  *      4.getName():获取当前线程的名字
  *      5.setName()：设置当前线程的名字
  *      6.yield():释放当前cpu的执行权(会切换线程),线程的礼让，但礼让的时间不确定，所以不一定礼让成功
- *      7.join():在线程a中调用线程b的join()，此时线程a进入阻塞状态，知道线程b完全执行完之后，a才结束阻塞状态
+ *      7.join():在线程a中调用线程b的join()，此时线程a进入WAITING状态，直到线程b完全执行完之后
  *      8.sleep(long 毫秒):线程休眠
  *      9.isAlive():判断线程是否存活
  *      10.getPriority():获得线程的优先级
